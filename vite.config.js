f494e948278602d987c0965cf82f2270ebb17a7c
lakshminarayana-qtx <lakshminarayana.podili@quadratyx.com>
2025-07-17 22:41:59 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/teams-notes-app/" // 👈 your repo name
-});
+  base: '/teams-notes-app/', // <-- This is required
+})
